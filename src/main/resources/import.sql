c88a86efa9da487a89bf621d354fbf2e40e8fb67
Manoel Campos <manoelcampos@gmail.com>
2023-05-09 16:35:23 -0300

    Introduz import.sql para popular o banco.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into marca (descricao) values ('LG'), ('Samsung');
+insert into modelo (descricao, marca_id) values ('abc123', 1), ('xyz', 2);
+insert into produto (descricao, preco, modelo_id, estoque) values ('TV 32', 2000, 1, 10);
+insert into produto (descricao, preco, modelo_id, estoque) values ('Ar condicionado', 1800, 2, 20);
