91dbc3ad84b86175e9c93468fbc0390f3a0a680c
Manoel Campos <manoelcampos@gmail.com>
2023-05-16 13:16:40 -0300

    Adiciona operações CRUD no ProdutoController. Adiciona métodos personalizados no ProdutoRepository
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/io/github/manoelcampos/lojavirtual/repository/ProdutoRepository.java
+++ src/main/java/io/github/manoelcampos/lojavirtual/repository/ProdutoRepository.java
@@ -2,9 +2,15 @@ package io.github.manoelcampos.lojavirtual.repository;
 
 import io.github.manoelcampos.lojavirtual.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProdutoRepository
-        extends JpaRepository<Produto, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findAllByModeloId(long id);
+
+    @Query("select p from Produto p where p.estoque > 0")
+    List<Produto> findAllProdutosEmEstoque();
 }
--- src/main/java/io/github/manoelcampos/lojavirtual/rest/ProdutoController.java
+++ src/main/java/io/github/manoelcampos/lojavirtual/rest/ProdutoController.java
@@ -3,9 +3,9 @@ package io.github.manoelcampos.lojavirtual.rest;
 import io.github.manoelcampos.lojavirtual.model.Produto;
 import io.github.manoelcampos.lojavirtual.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,8 +15,47 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository repository;
 
+    @PostMapping
+    public Long insert(@RequestBody Produto produto){
+        repository.save(produto);
+        return produto.getId();
+    }
+
+    @PutMapping
+    public void update(@RequestBody Produto produto){
+        repository.save(produto);
+    }
+
     @GetMapping("/all")
     public List<Produto> findAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produto findById(@PathVariable Long id){
+        System.out.println("findById: " + id);
+        return
+                repository
+                        .findById(id)
+                        .orElseThrow(
+                                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/modelo/{id}")
+    public List<Produto> findAllByModeloId(@PathVariable Long id) {
+        return repository.findAllByModeloId(id);
+    }
+
+    @GetMapping("/em-estoque")
+    public List<Produto> findAllProdutosEmEstoque() {
+        return repository.findAllProdutosEmEstoque();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        repository
+                .findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+        repository.deleteById(id);
     }
 }
